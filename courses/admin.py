d4c6718695f0752fcfc85bec97f2835725559142
Nittha Kaewsombut <6310611089@student.tu.ac.th>
2023-10-08 23:44:00 +0700

    update courses

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Course, CourseInfo
+
+class CourseInfoAdmin(admin.ModelAdmin):
+    list_display = ['course', 'semester', 'year', 'seat', 'course_status']
+
+admin.site.register(Course)
+admin.site.register(CourseInfo, CourseInfoAdmin)
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    course_id = models.CharField(max_length=10, unique=True)
+    course_name = models.CharField(max_length=60, unique=True)
+
+    def __str__(self):
+        return f'{self.course_id} (id={self.id})'
+    
+
+class CourseInfo(models.Model):
+    COURSE_STATUS = [
+        ('open', 'OPEN'),
+        ('close', 'CLOSE')
+    ]
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='subject')
+    semester = models.IntegerField()
+    year = models.IntegerField()
+    seat = models.IntegerField()
+    course_status = models.CharField(max_length=10, choices=COURSE_STATUS, default='open')
+
+    def __str__(self):
+        return f'{self.course} ({self.course_status})'
